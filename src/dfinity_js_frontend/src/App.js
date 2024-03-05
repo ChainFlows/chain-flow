79b4f4863bc0f44158d42b6665825c826e8acef3
45jerryjoseph <45jerryjoseph@gmail.com>
2024-03-05 15:39:36 +0300

    frontend routes

--- src/dfinity_js_frontend/src/App.js
+++ src/dfinity_js_frontend/src/App.js
@@ -1,57 +1,21 @@
-import React, { useEffect, useCallback, useState } from "react";
-import { Container, Nav } from "react-bootstrap";
+import React from "react";
 import "./App.css";
-import Wallet from "./components/Wallet";
-import coverImg from "./assets/img/sandwich.jpg";
-import { login, logout as destroy } from "./utils/auth";
-import { balance as principalBalance } from "./utils/ledger";
-import Cover from "./components/utils/Cover";
-import { Notification } from "./components/utils/Notifications";
 import HomeLandingpagePage from "./pages/HomeLandingpage";
 import "./styles/tailwind.css";
 import "./styles/index.css";
 import "./styles/font.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = function AppWrapper() {
-  const isAuthenticated = window.auth.isAuthenticated;
-  const principal = window.auth.principalText;
 
-  const [balance, setBalance] = useState("0");
-
-  const getBalance = useCallback(async () => {
-    if (isAuthenticated) {
-      setBalance(await principalBalance());
-    }
-  });
-
-  useEffect(() => {
-    getBalance();
-  }, [getBalance]);
+  
 
   return (
-    <>
-      <Notification />
-      {isAuthenticated ? (
-        <Container fluid="md">
-          <Nav className="justify-content-end pt-3 pb-5">
-            <Nav.Item>
-              <Wallet
-                principal={principal}
-                balance={balance}
-                symbol={"ICP"}
-                isAuthenticated={isAuthenticated}
-                destroy={destroy}
-              />
-            </Nav.Item>
-          </Nav>
-          <main>
-            <HomeLandingpagePage />
-          </main>
-        </Container>
-      ) : (
-        <Cover name="Street Food" login={login} coverImg={coverImg} />
-      )}
-    </>
+    <Router>
+        <Routes>
+          <Route path="/" element={<HomeLandingpagePage />} />
+        </Routes>
+    </Router>
   );
 };
 
